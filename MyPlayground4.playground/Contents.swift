d24330acb546008e6106cb2edeb4951bc4a7b908
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2020-01-16 03:51:43 +0900

    Update Contents.swift

--- MyPlayground4.playground/Contents.swift
+++ MyPlayground4.playground/Contents.swift
@@ -1,4 +1,18 @@
 import UIKit
 
-let n:Int=1...3
-print(n)
+//for
+//Output 1,2,3
+for number1 in 1...3{
+    print(number1)
+}
+
+//Output "apple" and "banana"
+let foods = ["apple","banana","grape"]
+for food in foods[..<2]{
+    print(food)
+}
+
+//Output "grape"
+for food in foods[2...]{
+    print(food)
+}
