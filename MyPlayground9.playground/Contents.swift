38c27797f107d57212fc378b8480531882054b86
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2020-02-28 11:34:04 +0900

    Add files via upload

---
+++ MyPlayground9.playground/Contents.swift
@@ -0,0 +1,11 @@
+//struct
+struct Fruits{
+    var apple:Int
+    var banana:Int
+    var grape:Int
+}
+
+let price = Fruits(apple: 100, banana: 200, grape: 300)
+print("Apple is \(price.apple) dollars.")
+print("Banana is \(price.banana) dollars.")
+print("Grape is \(price.grape) dollars.")
