39411c8a1d46142f467fdfdb5a51dd5b1af079a1
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2020-04-05 02:15:09 +0900

    Update ViewController.swift

--- MapKit/MapKit1/MapKit1/ViewController.swift
+++ MapKit/MapKit1/MapKit1/ViewController.swift
@@ -16,26 +16,26 @@ class ViewController: UIViewController,MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Mapviewの作成
+        //Mapviewの作成(Make Mapview)
         myMapKit = MKMapView()
         myMapKit.frame = self.view.bounds
         myMapKit.delegate = self
         self.view.addSubview(myMapKit)
         
-        //緯度と経度
+        //緯度と経度(Latitude and longitude)
         //東京駅(Tokyo Station)
         //緯度:35.681236,経度:139.767125
         let latitude: CLLocationDegrees = 35.681236
         let longitude: CLLocationDegrees = 139.767125
         let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
 
-        //縮尺の設定
+        //縮尺の設定(Setting the scale)
         var region = myMapKit.region
         region.center = location
         region.span.latitudeDelta = 0.02
         region.span.longitudeDelta = 0.02
 
-        //MapViewに反映
+        //MapViewに反映(Reflected in MapView)
         myMapKit.setRegion(region, animated: true)
     }
 
