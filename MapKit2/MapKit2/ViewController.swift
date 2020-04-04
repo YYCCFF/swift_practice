89c641129bbb98746317447f15d48aed3243ec92
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2020-04-05 02:17:30 +0900

    Update ViewController.swift

--- MapKit2/MapKit2/ViewController.swift
+++ MapKit2/MapKit2/ViewController.swift
@@ -7,20 +7,20 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
 
     @IBOutlet weak var myMapKit: MKMapView!
     
-     //CLLocationManagerの入れ物を用意
+     //CLLocationManagerの入れ物を用意(Prepare container for CLLocationManager)
     var myLocation:CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //CLLocationManagerをインスタンス化
+        //CLLocationManagerをインスタンス化(Instantiate CLLocationManager)
         myLocation = CLLocationManager()
         
-        //位置情報使用許可のリクエストを表示するメソッドの呼び出し
+        //位置情報使用許可のリクエストを表示するメソッドの呼び出し(Invoke method to display request for location information permission)
         myLocation.requestWhenInUseAuthorization()
     }
     
-     //位置情報取得に失敗したときに呼び出されるメソッド
+     //位置情報取得に失敗したときに呼び出されるメソッド(Method called when location information acquisition fails)
     func locationManager(_ maneger:CLLocationManager, didFailWithError error:Error){
         print("Error")
     }
